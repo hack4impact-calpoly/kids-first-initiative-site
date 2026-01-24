1a4e0c79c85df599d91965c5692f4b64f722d2c8
RishanBaweja <rbaweja@calpoly.edu>
2026-01-23 20:33:19 -0800

    feat: added schema for users and endpoints. Will test tomorrow

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,28 @@
+import connectDB from "@/database/db";
+import { NextResponse } from "next/server";
+import User from "@/database/userSchema";
+
+// CREATE a user with the parameters that are passed in, catching any server connection or bad requestst that may occur
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+
+    const body = await req.json();
+    const user = await User.create(body);
+    return NextResponse.json(user, { status: 201 });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 400 });
+  }
+}
+
+// RETRIEVE all users from the database, catching any errors that may occur
+export async function GET() {
+  try {
+    await connectDB();
+
+    const retrieved_users = await User.find({}).lean();
+    return NextResponse.json(retrieved_users, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 400 });
+  }
+}
