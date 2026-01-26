ab484906ca2e4734dd3296cd00b2b8886b2e5359
Theodore Weicker <tweickerv@gmail.com>
2026-01-25 22:23:35 -0800

    feat: made the styling for the login window better

--- src/app/login/[[...rest]]/page.tsx
+++ src/app/login/[[...rest]]/page.tsx
@@ -2,8 +2,20 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function LoginPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <SignIn />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      {/* This inner div scales the component up by 20% (1.2). 
+         You can change 1.2 to 1.3 or 1.5 if you want it even bigger.
+      */}
+      <div style={{ transform: "scale(1.2)" }}>
+        <SignIn path="/login" />
+      </div>
     </div>
   );
 }
