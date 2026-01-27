8a052dbc5b1fd7ff3e153005fd4f3dd6bf63589c
Nick E. <109779364+pbandjs1@users.noreply.github.com>
2026-01-27 03:36:28 -0800

    create navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,13 @@
+import { Link } from "@chakra-ui/react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <header className="navbar">
+      <nav className="navList">
+        <Link href="/home">Home</Link>
+        <Link href="/blog">Dashboard</Link>
+        <Link href="/portfolio">Profile</Link>
+      </nav>
+    </header>
+  );
 }
