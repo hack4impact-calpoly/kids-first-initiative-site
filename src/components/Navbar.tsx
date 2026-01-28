c00dcd8d7f944b604a194f4a55b1d5e0b5b38057
Nick E. <109779364+pbandjs1@users.noreply.github.com>
2026-01-27 19:15:18 -0800

    replace chakra-ui reference with basic html

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,10 @@
-import { Link } from "@chakra-ui/react";
-
 export default function Navbar() {
   return (
     <header className="navbar">
       <nav className="navList">
-        <Link href="/home">Home</Link>
-        <Link href="/blog">Dashboard</Link>
-        <Link href="/portfolio">Profile</Link>
+        <link href="/home">Home</link>
+        <link href="/blog">Dashboard</link>
+        <link href="/portfolio">Profile</link>
       </nav>
     </header>
   );
