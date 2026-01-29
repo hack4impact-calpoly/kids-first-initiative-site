55a1ae04dcba30a36948bd6c6373ef6bd2361e38
RishanBaweja <rbaweja@calpoly.edu>
2026-01-29 13:39:01 -0800

    feat: fixed up code to address comments

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,9 +1,9 @@
 import connectDB from "@/database/db";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import User from "@/database/userSchema";
 
 // CREATE a user with the parameters that are passed in, catching any server connection or bad requestst that may occur
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     await connectDB();
 
@@ -11,6 +11,7 @@ export async function POST(req: Request) {
     const user = await User.create(body);
     return NextResponse.json(user, { status: 201 });
   } catch (err: any) {
+    console.error("Caught an error", err);
     return NextResponse.json({ error: err.message }, { status: 400 });
   }
 }
@@ -23,6 +24,7 @@ export async function GET() {
     const retrieved_users = await User.find({}).lean();
     return NextResponse.json(retrieved_users, { status: 200 });
   } catch (err: any) {
+    console.error("Caught an error", err);
     return NextResponse.json({ error: err.message }, { status: 400 });
   }
 }
