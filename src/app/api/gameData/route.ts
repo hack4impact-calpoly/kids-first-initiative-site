4f3337bd5785ee6c84e510eb4d69b0ef0ed443da
ShortVincentMan <vinvyle06@gmail.com>
2026-02-09 22:51:04 -0800

    Built endpoints for POST PATCH and GET

--- src/app/api/gameData/route.ts
+++ src/app/api/gameData/route.ts
@@ -2,14 +2,15 @@ import connectDB from "@/database/db";
 import GameData from "@/database/gameDataSchema";
 import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params }: { params: { saveId: string } }) {
-  await connectDB();
+export async function POST(req: Request) {
+  try {
+    await connectDB();
 
-  const data = await GameData.findOne({ saveId: params.saveId });
-
-  if (!data) {
-    return NextResponse.json({ error: "Save not found" }, { status: 404 });
+    const body = await req.json();
+    const gamedata = await GameData.create(body);
+    return NextResponse.json(gamedata, { status: 201 });
+  } catch (err: any) {
+    console.error("Caught an error", err);
+    return NextResponse.json({ error: err.message }, { status: 400 });
   }
-
-  return NextResponse.json(data, { status: 200 });
 }
