0b364672c3cfee73b6025b287c600e3715b37172
overseer <108105465+OverseerUniverse@users.noreply.github.com>
2026-02-12 13:07:47 -0800

    Add two individual pages for each Unity game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
-import UnityIFrame from "@/components/UnityIFrame";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <h1>Home</h1>
-      <UnityIFrame game="PenguinRun" />
     </main>
   );
 }
