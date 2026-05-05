0a74a8648660a5f43e364452da7052814432a26c
justin@justinchan.org <jchan258@calpoly.edu>
2026-05-05 11:49:50 -0700

    feat: finished home page to route to logins

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import Navbar from "@/components/Navbar";
-import { Button } from "@chakra-ui/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main>
-      <Navbar />
-      <Button>example chakra button</Button>
-      <h1>Home</h1>
-    </main>
-  );
+  redirect("/home");
 }
